e3181770495ae29c4a629e6b2b83151a2d2d1909
osteriux <pixave73@gmail.com>
2025-02-12 09:20:23 +0100

    api beta

---
+++ api/src/functions/message.js
@@ -0,0 +1,23 @@
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        context.log(`Saying hello to ${name}`);
+
+        return { 
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                msg: `Hello, ${name}!`
+            })
+        };
+    }
+});
--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      console.log("fetching data");
+      fetch('/api/message?name=world')
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json);
+          setData(json.msg)
+        });
+    })();
+  });
+
+  return <div>Hello {data}</div>;
 }
 
 export default App;
